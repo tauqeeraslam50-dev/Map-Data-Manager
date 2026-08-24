e2c6fbf5d2ee5f637e1e80142276c73d10d2c296
tauqeeraslam50-dev <tauqeeraslam50@gmail.com>
2026-08-24 22:05:57 +0500

    Add satellite terrain and tile download
    * Add offline satellite terrain roots and bounded tile downloader
    
    * Expose satellite terrain and download APIs
    
    * Add map layers download controls and performance tuning
    
    * Add download destination picker
    
    * Expose download destination picker
    
    * Use explicit download destination and clean download bounds

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,7 +3,16 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   getTileServerUrl: () => ipcRenderer.invoke('tile-server:get-url'),
   selectOfflineFolder: () => ipcRenderer.invoke('offline:select-folder'),
+  selectSatelliteFolder: () => ipcRenderer.invoke('satellite:select-folder'),
+  selectTerrainFolder: () => ipcRenderer.invoke('terrain:select-folder'),
+  selectDownloadDestination: () => ipcRenderer.invoke('download:select-destination'),
   getOfflineFolder: () => ipcRenderer.invoke('offline:get-folder'),
+  getSatelliteFolder: () => ipcRenderer.invoke('satellite:get-folder'),
+  getTerrainFolder: () => ipcRenderer.invoke('terrain:get-folder'),
   scanOfflineFolder: () => ipcRenderer.invoke('offline:scan'),
+  scanSatelliteFolder: () => ipcRenderer.invoke('satellite:scan'),
+  scanTerrainFolder: () => ipcRenderer.invoke('terrain:scan'),
+  downloadTiles: (options) => ipcRenderer.invoke('download:tiles', options),
+  onDownloadProgress: (callback) => ipcRenderer.on('download:progress', (_event, progress) => callback(progress)),
   onTileServerReady: (callback) => ipcRenderer.on('tile-server-ready', (_event, port) => callback(port))
 });
